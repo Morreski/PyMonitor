8b00c4ff7906fa95999e659640511a28a7af894a
Enguerrand Pelletier <enguerrand.pelletier@gmail.com>
2016-06-09 08:07:41 +0200

    Updated view

--- website/scripts/cpu.js
+++ website/scripts/cpu.js
@@ -1,27 +1,57 @@
+var monitors = [];
+var disks = [];
 
 function cpu() {
-    var cpuDiv = document.getElementById("cpu-div");
-    var es = new EventSource("http://localhost:1337/api/cpu");
-    var monitors = [];
+  var cpuDiv = document.getElementById("cpu-div");
+  var es = new EventSource("http://localhost:1337/api/cpu");
 
-    es.addEventListener("message", function(evt){
+  es.addEventListener("message", function(evt){
+    data = JSON.parse(evt.data);
+    if(data.length != monitors.length) {
+
+      for(i in data) {
+        var meter = document.createElement("meter");
+        var value = document.createElement("p");
+        meter.max = 100;
+        cpuDiv.appendChild(meter);
+        cpuDiv.appendChild(value);
+        monitors.push([meter, value]);
+      }
+
+    }
+
+    for (i in data) {
+        monitors[i][0].value = data[i];
+        monitors[i][1].innerText = data[i];
+    }
+
+  })
+}
+
+function ram() {
+    var diskDiv = document.getElementById("disks-div");
+    var es = new EventSource("http://localhost:1337/api/disks");
+    es.onmessage = function(evt) {
         data = JSON.parse(evt.data);
-        if(data.length != monitors.length) {
-            for(i in data) {
-                var meter = document.createElement("meter");
-                var value = document.createElement("p");
-                meter.max = 100;
-                cpuDiv.appendChild(meter);
-                cpuDiv.appendChild(value);
-                monitors.push([meter, value]);
-            }
+        if(Object.keys(data).length != disks.length) {
+          for(_ in data) {
+            var meter = document.createElement("meter");
+            var value = document.createElement("p");
+            meter.max = 100;
+            diskDiv.appendChild(meter);
+            diskDiv.appendChild(value);
+            disks.push([meter, value]);
+          }
         }
 
-        for (i in data) {
-            monitors[i][0].value = data[i];
-            monitors[i][1].innerText = data[i];
+        var i = 0;
+        for(diskName in data) {
+          disks[i][0].value = data[diskName];
+          disks[i][1].innerText = diskName;
+          i++;
         }
-    })
+    }
 }
 
 cpu();
+ram();
